d47b1547eba47d3936ba202666c044d3336aab17
Aditi Singh <aditisingh9112007@gmail.com>
2026-07-30 11:45:03 +0530

    dom completed

---
+++ lab1/dom4.js
@@ -0,0 +1,9 @@
+import { EventEmitter } from "events";
+const form = new EventEmitter();
+
+form.on("submit",(uname,password) => {
+    console.log("form submitted");
+    console.log(`User password: ${password}`);
+});
+
+form.emit("submit","abc@abc.com","11223322");
---
+++ lab1/dom5.js
@@ -0,0 +1,27 @@
+import {EventEmitter} from "events";
+
+class DomClass extends EventEmitter{
+    addEventListener(eventName,callback){
+        this.on(eventName,callback);
+
+        }
+removeEventListener(eventName,callback){
+    this.off(eventName,callback);
+}
+dispatchEvent(eventName,eventData = {}) {
+    const event = {
+        type:eventName,
+        timespam:new Date(),
+        ...eventData,
+    };
+    this.emit(eventName,event);
+}
+    }
+const button = new DomClass();
+const handleclick = (event) => {
+    console.log(`button clicked type: ${event.tupe} at ${event.timespam}`);
+};
+button.addEventListener("click",handleclick);
+button.dispatchEvent("click", {
+    target:"submitBtn",
+});
